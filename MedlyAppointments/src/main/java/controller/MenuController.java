b9b013ffa8f4cb0c64d02169bed736d38ef05f10
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2026-05-19 19:34:57 -0700

    Logout implemented

--- MedlyAppointments/src/main/java/controller/MenuController.java
+++ MedlyAppointments/src/main/java/controller/MenuController.java
@@ -1,7 +1,14 @@
 package controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,9 +18,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(path = {"/menu", "/home"})
 public class MenuController {
-    
+
     @GetMapping
-    public String showMenu(){
+    public String showMenu() {
         return "home";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated()) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+            Cookie jwtCookie = new Cookie("jwt-token", null);
+            jwtCookie.setHttpOnly(true);
+            jwtCookie.setPath("/");
+            jwtCookie.setMaxAge(0);
+            response.addCookie(jwtCookie);
+            return "redirect:/index?logout=success";
+        } else
+            return "redirect:/index";
     }
 }
